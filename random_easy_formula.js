f8a16a789d5bcf69545d7ade603ad5d51d3c4a9b
MrigankaNaskar <mrigankanaskar4@gmail.com>
2024-03-22 11:12:38 +0530

    easy to learn

---
+++ random_easy_formula.js
@@ -0,0 +1,13 @@
+console.log(Math.random());
+console.log(Math.random() * 10 + 1);
+console.log(Math.floor(Math.random() * 10) + 1);
+
+
+const min = 10;
+const max = 30;
+
+console.log(Math.floor(Math.random() * (max - min)) + min);
+console.log(Math.floor(Math.random() * (max - min)) + min);
+console.log(Math.floor(Math.random() * (max - min)) + min);
+console.log(Math.floor(Math.random() * (max - min)) + min);
+console.log(Math.floor(Math.random() * (max - min)) + min);
