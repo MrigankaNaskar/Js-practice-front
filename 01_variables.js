d8b23f8ed4c0a0d32e4f393c3a8b9f3acbd7de38
MrigankaNaskar <mrigankanaskar4@gmail.com>
2024-04-26 14:43:24 +0530

    variables

---
+++ 01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 145563
+let accountEmail = "mrigo@google.com"
+var accountPassword = "56789"
+accountCity = "Kolkata"
+let accountState;
+// accountId = 5 // not allowed
+
+accountEmail = "ggsu@hhsg.com"
+accountPassword = "244535"
+accountCity = "Bangalore"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
