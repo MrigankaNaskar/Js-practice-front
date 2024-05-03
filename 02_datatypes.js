bbb83b3ae6b184fb0c9b6a14d9d4af2f9211a707
MrigankaNaskar <mrigankanaskar4@gmail.com>
2024-05-03 16:45:34 +0530

    datatypes

---
+++ 02_datatypes.js
@@ -0,0 +1,25 @@
+// "use strict"; // treat all JS code as newer version
+
+//alert(4 + 4)// we are using nodejs, not browser
+
+console.log(4 + 4)
+
+//console.log("Mriganka")
+
+//let name = Mriganka
+let age = 19
+let isloggedIn = false 
+let state;
+
+// number => 2 to power 56
+// bignit
+// string => ""
+// boolen => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+console.log(typeof "Mriganka");// string 
+console.log(typeof age);// number
+console.log(typeof null);// object
+console.log(typeof undefined);// undefined
