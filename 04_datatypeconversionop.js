2da1fedcaa3d10500d107e0290828054c2505d08
MrigankaNaskar <mrigankanaskar4@gmail.com>
2024-07-11 00:35:02 +0530

    datatype conversion operation

---
+++ 04_datatypeconversionop.js
@@ -0,0 +1,20 @@
+let value = 4
+let negValue = -value
+//console.log(negValue);
+
+let str1 = "hello"
+let str2 = " mriganka"
+
+let str3 = str1 + str2
+//console.log(str3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+ 
+num1 = num2 = num3 = 4 + 4
+
+let gameCounter = 100
+++gameCounter;
+//console.log(gameCounter);
