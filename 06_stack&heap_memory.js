b9676822bd1b65e6ac1240824ab285b3efd94783
MrigankaNaskar <mrigankanaskar4@gmail.com>
2024-08-29 21:44:20 +0530

    Stack and Heap memory

---
+++ 06_stack&heap_memory.js
@@ -0,0 +1,24 @@
+// Stack (Primitive) & Heap (Non - Primitive)
+
+
+let myMailname = "mrigankanaskardotcom"
+let anothername = myMailname
+
+anothername = "Mrigankax777"
+
+console.log(myMailname);
+console.log(anothername);
+
+
+let userOne = {
+    email: "user@google.com",
+    upi: "user@jkl"
+}
+
+let userTwo = userOne 
+
+userTwo.email = "mriganka@google.com"
+
+console.log(userOne.email);
+console.log(userOne.email);
+
